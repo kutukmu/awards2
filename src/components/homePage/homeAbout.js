50a7945d803399a7b0064808c9bf2b234a3a0f18
Kerim <kutukmu@gmail.com>
2020-06-30 00:03:33 -0500

    second

--- src/components/homePage/homeAbout.js
+++ src/components/homePage/homeAbout.js
@@ -113,7 +113,7 @@ const HomeAbout = ({ onCursor }) => {
               services.
             </h2>
             <p>
-              Everybody’s got a story. And we don’t stop until we’ve uncovered
+              Everyboddy’s got a story. And we don’t stop until we’ve uncovered
               what makes yours worth telling. Whether it’s working directly with
               you, an agency partner, or putting the finishing touches on
               something special, we’re ready to dig in and get our hands
